242cff7891cf181f58c9486a4d31c6353cb4e014
thien.thai <tthien@apcs.vn>
2015-12-16 12:02:09 +0700

    document test case test report

--- Mainscreen_Workchat/js/Test/testChatBubble.js
+++ Mainscreen_Workchat/js/Test/testChatBubble.js
@@ -1,8 +1,46 @@
 // testcase 1
 // Chat bubble group message of same user into a bubble
-var testcase1 = function (params) {
+
+
+var testcaseUC01_UI01 = function (params) {
 	var manager = new MessageManager();
-manager.receiveMessage("User_meow","Hello");
+manager.receiveMessage("Pusheen","Hello");
+manager.receiveMessage("Pusheen","It is nice day, isn't");
+manager.receiveMessage("Pusheen","Yes it is");
+}
+
+var testcaseUC01_UI02 = function (params) {
+	var manager = new MessageManager();
+manager.receiveMessage("Pusheen","Hello");
+manager.receiveMessage("Pusheen","It is nice day, isn't");
+manager.receiveMessage("Pusheen","Yes it is");
+manager.receiveMessage("Stormy","Hi");
+manager.receiveMessage("Stormy","It is nice day, isn't");
+manager.receiveMessage("Stormy","Yes it is");
+}
+
+var testcaseUC01_UI03 = function (params) {
+	var manager = new MessageManager();
+manager.receiveMessage("Pusheen","Hello");
+manager.receiveMessage("Stormy","Hi");
+manager.receiveMessage("Pusheen","It is nice day, isn't");
+manager.receiveMessage("Stormy","Yes it is");
+manager.receiveMessage("Pusheen","Let's play");
+}
+
+var testcaseUC01_UI04 = function (params) {
+	var manager = new MessageManager();
+manager.receiveMessage("Pusheen","Hello");
+manager.receiveMessage("Pusheen","It is nice day, isn't");
+manager.receiveMessage("Stormy","Yes it is");
+manager.receiveMessage("Stormy","It is nice day, isn't");
+}
+
+testcaseUC01_UI04();
+
+var testcase = function (params) {
+	var manager = new MessageManager();
+manager.receiveMessage("pusheen","Hello");
 manager.receiveMessage("User_meow","It is nice day, isn't");
 manager.receiveMessage("User_woof","Yes it is");
 manager.receiveMessage("User_woof","Keep calm and woof on");
@@ -16,4 +54,4 @@ manager.receiveMessage("abc","meow meow ?");
 manager.receiveMessage("abc","meow meow ?");
 }
 
-testcase1();
+
